4343997c401082d0fc4664c1f552fab5c89f9030
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-12 14:22:18 +0100

    dropdown done

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+const InfoView = require('./views/info_view');
 const Countries = require('./models/countries');
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
 
 const countries = new Countries;
-countries.getAllCountriesFromApi();
+countries.bindEvents();
 
-
+const dropdown = document.querySelector('#countries')
+const infoView = new InfoView(dropdown);
+infoView.bindEvents();
 
 
 
--- src/models/countries.js
+++ src/models/countries.js
@@ -1,16 +1,40 @@
 const Request = require('../helpers/request');
+const PubSub = require('../helpers/pub_sub');
 
 const Countries = function(){
-    this.countriesData = null;
+    this.countriesData = [];
 }
 
+Countries.prototype.bindEvents= function(){
+    this.getAllCountriesFromApi();
+};
+
+
 Countries.prototype.getAllCountriesFromApi = function(){
-    
     const request = new Request('https://restcountries.eu/rest/v2/all');
     request.get((data) => {
         this.countriesData = data;
-
+        this.publishCountryNames();
+        this.publishCountryNativeNames();
     })
+};
+
+
+Countries.prototype.publishCountryNames = function(){
+    const onlyNames = [];
+    this.countriesData.forEach(country => {
+        onlyNames.push(country.name);
+    });
+    PubSub.publish('Countries:names-ready', onlyNames);
+};
+
+Countries.prototype.publishCountryNativeNames = function (){
+    const onlyNativeNames = [];
+    this.countriesData.forEach(country => {
+        onlyNativeNames.push(country.nativeName);
+
+    });
+    PubSub.publish('Countries:native-names-ready', onlyNativeNames);
 };
 
 
