44c3b4d3fe0845c44411ea7476a54f480509d5b8
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-12 15:29:16 +0100

    mvp + extensions done

--- src/views/info_view.js
+++ src/views/info_view.js
@@ -15,7 +15,27 @@ InfoView.prototype.display = function (countryData){
     const title = document.createElement('h1');
     title.textContent = countryData.nativeName;
     this.htmlElement.appendChild(title);
-    console.log(this.htmlElement);
+
+    const flag = document.createElement('img');
+    flag.src = countryData.flag;
+    this.htmlElement.appendChild(flag);
+
+    const region = document.createElement('h3');
+    region.textContent = `Region: ${countryData.region}`;
+    this.htmlElement.appendChild(region);
+
+    const languagestitle = document.createElement('h3');
+    languagestitle.textContent = 'Languages:';
+    this.htmlElement.appendChild(languagestitle);
+
+    const langUl = document.createElement('ul');
+    this.htmlElement.appendChild(langUl);
+
+    countryData.languages.forEach(language => {
+        const langLi = document.createElement('li');
+        langLi.textContent = language.name;
+        this.htmlElement.appendChild(langLi);
+    });
 
 }
 
